52ae0b136dda5210893d5274cccba626c52edaf5
Alison Rodrigues <alisonrodrigues@ufgd.edu.br>
2021-08-12 16:06:11 -0400

    feat: entrypoint da aplicação

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import staticServer from "./staticServer"
+import apiServer from "./apiServer"
+import dotenv from 'dotenv'
+
+
+// SE ESTIVER RODANDO EM PRODUÇÃO OU OUTRO AMBIENTE 
+// AS VARIÁVEIS DE AMBEINTE JÁ ESTARAM CONFIGURADAS
+if (process.env.NODE_ENV?.toLocaleLowerCase() === 'dev') {
+    dotenv.config()   
+}
+
+
+staticServer.listen(process.env.PORT).on('listening', () => {
+    console.log(`Site rodando em http://localhost:${process.env.PORT}`)
+})
+
+apiServer.listen(process.env.API_PORT, () => {
+  console.log(`API rodando em http://localhost:${process.env.API_PORT}`)
+})
